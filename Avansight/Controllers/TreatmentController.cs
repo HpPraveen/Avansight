50218683516e81a1b38d40410cb2af60921cbeef
Pasindu Praveen Howpage <pasindu@mimobimedia.com>
2021-06-04 10:02:59 +0530

    add

---
+++ Avansight/Controllers/TreatmentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Avansight.Business_Layer;
+
+namespace Avansight.Controllers
+{
+    public class TreatmentController : Controller
+    {
+        private readonly TreatmentService _treatmentService;
+
+        public TreatmentController(TreatmentService treatmentService)
+        {
+            _treatmentService = treatmentService;
+        }
+        public IActionResult Index()
+        {
+            return View(null);
+        }
+    }
+}
